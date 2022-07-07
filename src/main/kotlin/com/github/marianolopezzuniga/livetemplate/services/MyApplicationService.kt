8f0f9374daf3b25628985be6f889a81ab40d3ac2
GitHub Action <action@github.com>
2022-07-07 08:14:54 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/marianolopezzuniga/livetemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.marianolopezzuniga.livetemplate.services
+
+import com.github.marianolopezzuniga.livetemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
